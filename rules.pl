4a811df492ada0d16f208f826757a11f0076c9d7
Kirk Hardy <megal33tdude@gmail.com>
2017-11-03 03:02:37 -0400

    cleanup

--- rules.pl
+++ rules.pl
@@ -15,31 +15,35 @@ greatgrandparent(GG,C):-
     parent(X2,X),
     parent(GG,X2).
 
-/* first cousins */
+/* first cousins, the base case */
 nthcousin(A,B,1):-
     parent(P1,A),
     parent(P2,B),
     sibling(P1,P2).
 
 /* recursively define nth-cousins */
-/*  */
 nthcousin(A,B,N):-
     parent(P1,A),
     parent(P2,B),
     I is N-1,
     nthcousin(P1,P2,I).
 
+/* nth/k-removed base case */
 nthcousinkremoved(A,B,N,0):-
     nthcousin(A,B,N).
 
+/* recursive definition */
 nthcousinkremoved(X,Y,N,K):-
     parent(P_maybe, Y),
     I is K-1,
     nthcousinkremoved(X,P_maybe,N,I).
 
+/* get descendants of a parent at a specific level */
+/* base case */
 kthlevel_child(X,Y,0):-
     child(Y,X).
 
+/* recursive definition */
 kth_levelchild(X,Y,K):-
     child(Z,X),
     I is K-1,
@@ -49,6 +53,7 @@ kth_levelchild(X,Y,K):-
 children(P,Cn):-
     findall(X,child(X,P),Cn). 
 
+/* children of P but sorted by age() */
 sortedchildren(P,ByAge):-
     children(P,Cn),
     map_list_to_pairs(age,Cn,Pairs),
@@ -56,18 +61,9 @@ sortedchildren(P,ByAge):-
     reverse(SortedBackwards,Sorted),
     pairs_values(Sorted, ByAge).
 
-older(X,Y):-
-    age(X,A1),
-    age(Y,A2),
-    A1>A2.
-
-firstchild(P,First):-
-    children(P,[First|_]).
-
-secondchild(P,Second):-
-    children(P,[_,Second|_]).
-
+/* kth-child of P, pass K starting at 0 */
+/* so k=0 for first child, k=1 for second, etc */
 kthchild(P,K,KthChild):-
     sortedchildren(P,ByAge),
-    %N is K-1,
-    nth0(N,ByAge,KthChild).
+    nth0(K,ByAge,KthChild).
+
