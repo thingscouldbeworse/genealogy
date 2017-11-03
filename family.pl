fe45b9abb54acfd1907f372212ca886972a7a7a9
Kirk Hardy <megal33tdude@gmail.com>
2017-11-02 22:09:51 -0400

    nthcousinkremoved works now

--- family.pl
+++ family.pl
@@ -42,7 +42,7 @@ child(michael, liz).
 /* johanna family */
 age(mike, 41).
 age(wesley, 13).
-age(cally, 10).
+age(callie, 10).
 child(wesley, johanna).
 child(cally, johanna).
 
@@ -67,20 +67,38 @@ sibling(A,B):-
     child(A,P),
     child(B,P),
     A\=B.
+
 parent(P,C):-child(C,P).
+
 grandparent(G,C):-
     parent(X,C),
     parent(G,X).
+
 greatgrandparent(GG,C):-
     parent(X,C),
     parent(X2,X),
     parent(GG,X2).
-firstcousin(A,B):-
+
+/* first cousins */
+nthcousin(A,B,1):-
     parent(P1,A),
     parent(P2,B),
     sibling(P1,P2).
-firstcousinonceremoved(A,B):-
-    child(A,C1),
-    firstcousin(C1,B).
+
+/* recursively define nth-cousins */
+/*  */
+nthcousin(A,B,N):-
+    parent(P1,A),
+    parent(P2,B),
+    I is N-1,
+    nthcousin(P1,P2,I).
+
+nthcousinkremoved(A,B,N,0):-
+    nthcousin(A,B,N).
+
+nthcousinkremoved(X,Y,N,K):-
+    parent(P_maybe, Y),
+    I is K-1,
+    nthcousinkremoved(X,P_maybe,N,I).
 
 
